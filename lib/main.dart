ef81e5539f63510ae411c711932a3633f9fb27ef
Ankit <ankitumredkar77@gmail.com>
2024-07-05 22:25:05 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LightToDarkTheme(),
+      home: Example2(),
     );
   }
 }
